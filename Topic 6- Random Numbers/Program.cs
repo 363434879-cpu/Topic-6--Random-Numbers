b13110ccc1186b96a6ba6f90c5e45282dfd5dc54
363434879 <363434879@gotvdsb.ca>
2026-03-05 12:53:55 -0500

    Add project files.

---
+++ Topic 6- Random Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_6__Random_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
